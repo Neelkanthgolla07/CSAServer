2f8b3582499fdceda6e2d9ffd13fd4a0c143b016
Neelkanth golla <gollaneelkanth@gmail.com>
2025-05-12 11:02:37 +0530

    added frontend url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app=express();
 app.use(express.json())
 // app.use(cors())
 app.use(cors({
-    origin: ['http://localhost:5173', 'YOUR_DEPLOYED_FRONTEND_URL'],
+    origin: ['http://localhost:5173', 'https://csa-client-zeta.vercel.app'],
     credentials: true
 }));
 
